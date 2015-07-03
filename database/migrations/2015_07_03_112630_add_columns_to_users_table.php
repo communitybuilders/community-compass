18c276a448722ee77f99c701dadc1f4cc0eea0db
Andrew Wong <andrew.wong@socialengineers.com.au>
2015-07-03 21:38:13 +1000

    Add more columns to users table.

---
+++ database/migrations/2015_07_03_112630_add_columns_to_users_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddColumnsToUsersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->renameColumn('name', 'username');
+            $table->string('first_name');
+            $table->string('last_name');
+            // TODO: set foreign key once table is up.
+            $table->integer('user_type_id');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->renameColumn('username', 'name');
+            $table->dropColumn('first_name');
+            $table->dropColumn('last_name');
+            $table->dropColumn('user_type_id');
+        });
+    }
+}
