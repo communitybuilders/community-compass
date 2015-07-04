f02a969d14bf530c304e6a46c75107181e4db4d5
Andrew Wong <wongadev@gmail.com>
2015-07-04 21:43:10 +1000

    Move js to resources

--- gulpfile.js
+++ gulpfile.js
@@ -24,6 +24,6 @@ elixir(function(mix) {
         'public/**/*',
         'resources/views/**/*'
     ], {
-        proxy: 'communitycompass.localhost'
+        proxy: 'dev.community-compass.192.168.22.10.xip.io'
     });
 });
