b8e7b8a40023e4fce35fd438233d64fb947a6486
Dejan <dejan@legalconsult.com.au>
2015-07-05 15:40:29 +1000

    Updated infinite scroll divs to match height of page load divs.

--- public/js/all.js
+++ public/js/all.js
@@ -187,12 +187,12 @@ $(function () {
 
                         $(result).each(function(index, value){
                             display +="<div class='col-xs-6 col-md3 col-md-3 col-lg-3' style='float:left;'>";
-                            display +="<div class='panel panel-default' style='height:450px'>";
+                            display +="<div class='panel panel-default' style='height:500px'>";
                             if (value.image_uri != ''){
                                 display +="<img class='img-responsive center-block' style='width:100%; height:252px' src=" + value.image_uri +" alt='image'><br />";
                             }else{
                                 display +="<img class='img-responsive center-block' style='width:100%' src='data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAKAAAACgCAMAAAC8EZcfAAAANlBMVEXMzMzPz8+RkZHExMSampqQkJDKysqfn5/Hx8e7u7vDw8OUlJS4uLiXl5esrKypqamzs7Ojo6OgzyMdAAACGUlEQVR4nO3V247cIBAEUJqLAXP//59NNR5npd1IeQl5qiNZYhgsaqBhjCEiIiIiIiIiIiIiIiIiIiIiIiIiIiIiOklE3sa3jp8D5fsbfx75T+URe9LJ8lp1zzrjuv80cV9r7v4eh75hUn9ePWraFot3aBTvgybs1rfwc96EYd52tNAoHgOcb9Gu62y+5H2WbqekVVL2XsztY5plvJv57rxMOy6MviWj0csU9EwTWz4b8Mq3MRUBkQshQ5LaOj74vTB1TNFH264mIxEBNdxth5GFDxryuCuGLFkXbdgss1S5EARfyAj+XsG/BYnw/pLVsiS7krT4LOfxgLF0zG27YK91v6vIExC9Zfjx+8BkX25dxWwkLCcBAd35gFfHaug274D1DfgprVba/Y68Izb3E9DGHdD8h4A4vXqp5NKfLdaA5rOCBgX3FSCWoV1Lz4XFChas4H084O3DXqzchlzDouyxTDit7gn4XCi7OfdKozBRg05rUI903RfPQZdeMXqRYP9EHyRdos8OpTXY5fNLkEd20L4fGThOo+SzfybJt1lrxTb10gemNddqfdn8hnKIsE/0bDHXiv7U/PRambnFWaI7ms+4YgNgujRs2f9gbtk29pe4Zga2POg5Rm3qSFw5kqNt+/arPsTD9zTibNq6nHvqPb0NtJJ2p0/vOzK59PU9EREREREREREREREREREREREREREREf3NL2UnEmIOKkswAAAAAElFTkSuQmCC' alt='image'><br />";
-                            }display +="<div style='padding-left:17px; padding-right: 17px;'>"
+                            }display +="<div id='org_actions' style='padding-left:17px; padding-right: 17px;'>"
                             display += value.legal_name + "<br />";
                             if ($('#auth').val().length) {
                                 display +="<a href='#' id='claim-{{$value->id}}' name='claim' data-toggle='modal' data-target='#claim' class='claim_link'><span class='fa fa-plus-square'> Claim</span></a><br />";
