359c7ed534df728c6ddc8609aa5cdf6214619735
Andrew Wong <andrew.wong@legalconsult.com.au>
2015-07-08 12:55:19 +1000

    Use icons for map info window links

--- public/js/all.js
+++ public/js/all.js
@@ -76,7 +76,7 @@ $(function () {
             var infoContainer = $('.info-window').first();
 
             var content = '<div class="info-window">' +
-                '<div class="image-container pull-left">';
+                '<div class="image-container pull-left" style="margin-right: 1em;">';
 
             if (orgInfo.image) {
                 content += '<img style="max-height: 100px; max-width: 100px;" src=' + orgInfo.image.image_uri + '>';
@@ -85,23 +85,27 @@ $(function () {
             }
 
             content += '<div class="info-actions text-center">' +
-                '<a href="info-claim">C</a>' +
-                '<a href="info-like">L</a>' +
-                '<a href="info-subscribe">S</a>' +
-                '<a href="info-donate">D</a>' +
+                ' <a href="#" title="Claim" class="info-claim"><i class="fa fa-plus-square"></i></a>' +
+                ' <a href="#" title="Like" class="info-like"><i class="fa fa-heart"></i></a>' +
+                ' <a href="#" title="Share" class="info-share"><i class="fa fa-share-square-o"></i></a>' +
+                ' <a href="#" title="Donate" class="info-donate"><i class="fa fa-usd"></i></a>' +
+                ' <a href="#" title="Volunteer" class="info-volunteer"><i class="fa fa-group"></i></a>' +
+                ' <a href="#" title="Partner" class="info-partner"><i class="fa fa-exchange"></i></a>' +
+                ' <a href="#" title="Contact" class="info-contact"><i class="fa fa-envelope"></i></a>' +
                 '</div>' +
                 '</div>' +
                 '<div class="pull-left">' +
                 '<p class="info-legal-name">' + orgInfo.legal_name + '</p>';
 
+            content += '<p><span class="info-address.address-1">' + orgInfo.address_line_1 + ' ' + orgInfo.address_line_2 + ' ' + orgInfo.address_line_3 + '</span><br>' +
+                '<span class="info-address.address-2">' + orgInfo.suburb + ' ' + orgInfo.state + ' ' + orgInfo.state + '</span>' +
+                '</p>';
+
             if (orgInfo.website) {
                 content += '<p class="info-website"><a href="' + orgInfo.website.url + '">' + orgInfo.website.url + '</a></p>';
             }
 
-            content += '<p><span class="info-address.address-1">' + orgInfo.address_line_1 + ' ' + orgInfo.address_line_2 + ' ' + orgInfo.address_line_3 + '</span><br>' +
-                '<span class="info-address.address-2">' + orgInfo.suburb + ' ' + orgInfo.state + ' ' + orgInfo.state + '</span>' +
-                '</p>' +
-                '</div>' +
+            content += '</div>' +
                 '</div>';
 
             infoWindow.setContent(content);
